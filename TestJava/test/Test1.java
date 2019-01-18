efbf98591db03b30467908f2ec3730ce018ff123
lxycomeon <lxycomeon@163.com>
2019-01-18 16:59:03 +0800

    2019/01/18

--- TestJava/test/Test1.java
+++ TestJava/test/Test1.java
@@ -3,12 +3,12 @@ package test;
 public interface Test1 {
 	public static Integer ss=0;
 	void name();
-	public void main(String[] aStrings);
-    public static boolean setFlag() {
-    	System.out.println("21");
-		return false;
-	}
-	
+
+//    public static boolean setFlag() {
+//    	System.out.println("21");
+//		return false;
+//	}
+//	
 //	public default void main(String[] args) {
 //	
 //		System.out.println("2");
